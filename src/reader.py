4be1828b469bda38c166d73e4645635142a5d0ab
Aleksei <aleksei.kopolov@gmail.com>
2019-04-23 02:23:12 +0300

    reader update

--- src/reader.py
+++ src/reader.py
@@ -1,54 +1,61 @@
 import urllib.request
+import time
 
 import numpy as np
-
-from src.util import progress
+import cv2 as cv
 
 from requests_html import HTMLSession
 from typing import List
 from PIL import Image
 
+from src.util import progress
+from src.util import get_links_from_file
+
 class Reader(HTMLSession):
-    def __init__(self) -> None:
+    def __init__(self, img_width=256, img_height=256, batches=5, epochs=2)-> None:
         super().__init__()
         self.base_url = "http://www.dpchallenge.com/image.php?IMAGE_ID="
-        self.img_link_map = {}
+        self.img_width = img_width
+        self.img_height = img_height
+        self.batches = batches
+        self.epochs = epochs
 
-    def read(self, txt_path:str=None, img_id_list:List[str]=None, img_id:str=None)-> np.ndarray:
-        if txt_path != None:
-            return self.read_from_txt(txt_path=txt_path)
-        
-        elif img_id_list != None:
-            return self.read_from_list(img_id_list=img_id_list)
+    def read(self, path:str=None, img_id:str=None)-> np.ndarray:
+        if path != None:
+            return self.read_from_txt(path=path)
         
         elif img_id != None:
             return self.read_from_image_id(img_id=img_id)
     
-    def read_from_txt(self, txt_path:str)-> np.ndarray:
-#         progress(i+1, total, status='Downloading')
-        pass
-    
-    def read_from_list(self, img_id_list:List[str])-> np.ndarray:
-        total = len(img_id_list)
-        for i in range(total):
-            progress(i+1, total, status='Downloading images')
-            pass
+    def read_from_txt(self, path:str)-> np.ndarray:
+        for epoch in range(self.epochs):
+            images = []
+            item = 0
+            img_links = get_links_from_file(path=path, batches=self.batches, epoch=epoch)
+            start_time = time.time()
+            for link in img_links:
+                image = self.get_image_from_link(link=link)
+                images.append(image)
+                item += 1
+                progress(item, self.batches, status="Downloading Images")
+            print("\nDownloading time: {0}s".format(round(time.time() - start_time, 2)))
+            yield np.array(images)
+
     
     def read_from_image_id(self, img_id:str)-> np.ndarray:
-        return np.array([self.get_image(img_id=img_id)])
-        
-    def get_image(self, img_id:str)-> np.ndarray:
-        img_link = None
-        
-        if img_id not in self.img_link_map:
-            img_link = self.parse_html(img_id)
-        else:
-            img_link = self.img_link_map[img_id]
-
-        with urllib.request.urlopen(img_link) as response:
-            image = Image.open(response)
-            return np.asarray(image)
+        return np.array([self.get_image_by_id(img_id=img_id)])
     
+    def get_image_by_id(self, img_id:str)-> np.ndarray:
+        link = self.parse_html(img_id)
+        return self.get_image_from_link(link=link)
+    
+    def get_image_from_link(self, link:str)-> np.ndarray:
+        with urllib.request.urlopen(link) as response:
+            img = np.asarray(bytearray(response.read()), dtype="uint8")
+            img = cv.imdecode(img, cv.IMREAD_COLOR)
+            img = cv.resize(img, (self.img_width, self.img_height))
+            return np.asarray(img)
+        
     def parse_html(self, img_id:str)-> str:
         url = self.base_url + img_id
         response = self.get(url)
--- src/util.py
+++ src/util.py
@@ -1,5 +1,8 @@
 import sys
-import time
+
+from typing import List
+
+base_img_url = "https://images.dpchallenge.com/images_challenge/"
 
 def progress(count, total, status=''):
     bar_len = 60
@@ -10,3 +13,30 @@ def progress(count, total, status=''):
 
     sys.stdout.write('[%s] %s%s ...%s\r' % (bar, percents, '%', status))
     sys.stdout.flush()
+    
+def get_challenge_range(challenge:int) -> str:
+    if challenge < 1000:
+        return "0-999/"
+    elif challenge < 2000:
+        return "1000-1999/"
+    elif challenge < 3000:
+        return "2000-3999/"
+    
+def get_links_from_file(path:str, batches:int, epoch:int) -> List[str]:
+    url_list = []
+    print("Dataset setting up epoch: {0}".format(epoch))
+    with open(path, "r") as file:
+        item = 0
+        for line in file.readlines()[epoch*batches:(epoch+1)*batches]:
+            values = line.split(" ")
+            challange = int(values[14])
+            img_id = values[1]
+            url = base_img_url + get_challenge_range(challange) + \
+                    "{0}/".format(challange) + "1200/Copyrighted_Image_Reuse_Prohibited_" + \
+                    img_id + ".jpg"
+            url_list.append(url)
+            item += 1
+            progress(item, batches, status="Getting links")
+            if item == batches:
+                return url_list
+                
